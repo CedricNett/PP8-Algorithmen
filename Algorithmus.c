acb02ebec5fdf2e46499fd4cc06f9fbbb69a1c78
cedric <cedricnettler@gmail.com>
2022-06-06 12:46:31 +0200

    Hier ist das Programm zum Pseudocode

---
+++ Algorithmus.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+	int a[]={1,56,5,3,8,6,0,34,4,6,87,2,1};
+
+	int minimum = a[0];
+
+	for(int i=0; a[i]!=0; i++)	{
+
+		if (a[i] < minimum) minimum = a[i];
+		}
+
+	printf("Die kleinste Zahl im Array (56,5,3,8,6,0,34,4,6,87,2) ist die Zahl %i \n", minimum);
+
+	return 0;
+	}
